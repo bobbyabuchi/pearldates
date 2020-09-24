940d9fa32843e9d32eef1fb09226d504fc48effc
Godswill C. Abuchi <markachi@icloud.com>
2020-09-24 15:10:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pearl Dates | Home</title>
+		<?php include 'includes/links.php'; ?>
+	</head>
+	<body>
+		<div class="container" align="center">
+
+				<div class="header">
+					<?php include 'includes/header.php' ?>
+				</div>
+
+				<div class="navi">
+					<?php include 'includes/nav.php' ?>
+				</div>
+
+				<div class="stats">
+					 <?php include 'pages/statshome.php' ?>	
+				</div>
+
+				<div class="footer">
+					<?php include 'includes/footer.php' ?>
+				</div>
+
+		</div>
+	</body>
+</html>
